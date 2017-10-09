80ce4b94df69d48d6073565c78303ae14090c43d
HUNG <HUNG@HUNG-HP>
2017-10-08 21:40:29 -0400

    PS3 Finished

--- PS3/src/main/java/pkgCore/Deck.java
+++ PS3/src/main/java/pkgCore/Deck.java
@@ -1,14 +1,17 @@
 package pkgCore;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.*;
 
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
+import pkgException.DeckException;
 
 public class Deck {
 
-	private ArrayList<Card> cardsInDeck;
+	private ArrayList<Card> cardsInDeck = new ArrayList();
 
 	public Deck() {
 		for (eSuit eSuit : eSuit.values()) {
@@ -19,19 +22,70 @@ public class Deck {
 		Collections.shuffle(cardsInDeck);
 	}
 
-	//TODO: Fix the Draw method so it throws an exception if the deck is empty
 	public Card Draw() {
-		return cardsInDeck.remove(0);
+		try {
+			if (cardsInDeck.size()==0)
+				throw new DeckException(this);
+			
+			return cardsInDeck.remove(0);
+		}
+		catch (DeckException e) {
+			System.out.println("DeckException: There are no more cards in the deck.");
+			return null;
+		}
 	}
 	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eSuit
-	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eRank
+	public Card Draw(eSuit eSuit) {
+		Card card = cardsInDeck.stream()
+				.filter(c->c.geteSuit() == eSuit)
+				.findFirst()
+				.orElse(null);
+		cardsInDeck.remove(card);
+		
+		if (card == null)
+			System.out.println("There are no more of this suit.");
+		
+		return card;
+	}
 
-	//TODO: Write a method that will return the number of a given eSuit left in the deck.
+	public Card Draw(eRank eRank) {
+		Card card = cardsInDeck.stream()
+				.filter(c->c.geteRank() == eRank)
+				.findFirst()
+				.orElse(null);
+		cardsInDeck.remove(card);
+		
+		if (card == null)
+			System.out.println("There are no more of this rank.");
+			
+		return card;
+	}
 	
-	//TODO: Write a method that will return the number of a given eRank left in the deck.
+	public int deckSuitCount(eSuit eSuit) {
+		int remaining = (int) cardsInDeck.stream()
+				.filter(c->c.geteSuit() == eSuit)
+				.count();
+		
+		return remaining;
+	}
 	
-	//TODO: Write a method that will return 0 or 1 if a given card is left in the deck.
+	public int deckRankCount(eRank eRank) {
+		int remaining = (int) cardsInDeck.stream()
+				.filter(c->c.geteRank() == eRank)
+				.count();
+		
+		return remaining;
+	}
+	
+	public int deckCardCount(Card Card) {
+		int remaining = (int) cardsInDeck.stream()
+				.filter(c->c == Card)
+				.count();
+		
+		if (remaining > 1) // in case, more than 1 deck are used
+			remaining = 1;
+		
+		return remaining;
+	}
 	
 }
--- PS3/src/test/java/pkgCore/DeckTest.java
+++ PS3/src/test/java/pkgCore/DeckTest.java
@@ -4,29 +4,88 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+import pkgException.DeckException;
+
 public class DeckTest {
 
 	@Test
-	public void TestEmptyDeck() {
-		//TODO: Build a deck, draw until you get a DeckException
+	public void TestEmptyDeck() throws DeckException{
+		Deck deck = new Deck();
+		
+		//Draws 51 cards, so there is only 1 left
+		for (int i=0; i < 51; i++)
+			deck.Draw();
+		
+		//draws and tests last card
+		assertEquals(deck.Draw()==null,false);
+		
+		//try to draw an empty deck
+		assertNull(deck.Draw());
 	}
 	
 	@Test
 	public void TestDrawSuit() {
-		//TODO: Build a deck, test the Draw(eSuit) method
+		Deck deck = new Deck();
+		
+		assertEquals(eSuit.CLUBS, deck.Draw(eSuit.CLUBS).geteSuit());
+		assertEquals(eSuit.DIAMONDS == deck.Draw(eSuit.CLUBS).geteSuit(), false);
+		
+		// loop removes rest of CLUBS for next test
+		for(int i=0;i<11;i++)
+			deck.Draw(eSuit.CLUBS);
+		
+		assertEquals(deck.Draw(eSuit.CLUBS),null);
 	}
 	
 	@Test
 	public void TestDrawRank() {
-		//TODO: Build a deck, test the Draw(eRank) method
+		Deck deck = new Deck();
+		
+		assertEquals(eRank.ACE, deck.Draw(eRank.ACE).geteRank());
+		assertEquals(eRank.EIGHT == deck.Draw(eRank.ACE).geteRank(), false);
+		
+		// loop removes the rest of the ACEs for next test
+		for(int i=0;i<2;i++)
+			deck.Draw(eRank.ACE);
+		
+		assertEquals(deck.Draw(eRank.ACE),null);
 	}
 	
+	@Test
 	public void TestDeckRankCount() {
-		//TODO: Build a deck, test the DeckRankCount method
+		Deck deck = new Deck();
+		
+		assertEquals(deck.deckRankCount(eRank.EIGHT), 4);
+		
+		deck.Draw(eRank.EIGHT);
+		deck.Draw(eRank.EIGHT);
+		
+		assertEquals(deck.deckRankCount(eRank.EIGHT), 2);
+		
+		deck.Draw(eRank.EIGHT);
+		deck.Draw(eRank.EIGHT);
+		
+		assertEquals(deck.deckRankCount(eRank.EIGHT), 0);
+		
 	}
 	
+	@Test
 	public void TestDeckSuitCount() {
-		//TODO: Build a deck, test the DeckSuitCount method
+		Deck deck = new Deck();
+		
+		assertEquals(deck.deckSuitCount(eSuit.CLUBS), 13);
+		
+		for (int i = 0; i<6; i++)
+			deck.Draw(eSuit.CLUBS);
+		
+		assertEquals(deck.deckSuitCount(eSuit.CLUBS), 7);
+		
+		for (int i = 0; i<7; i++)
+			deck.Draw(eSuit.CLUBS);
+		
+		assertEquals(deck.deckSuitCount(eSuit.CLUBS), 0);
 	}
 
 }
